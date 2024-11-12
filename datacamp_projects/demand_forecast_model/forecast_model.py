d45406ac8fff29bd48cff3c5bc12873b96b644bc
Gui-Amaral2003 <amaralguilherme213@gmail.com>
2024-11-12 13:44:26 -0300

    adicionando o paramGrid no forecast_model

--- datacamp_projects/demand_forecast_model/forecast_model.py
+++ datacamp_projects/demand_forecast_model/forecast_model.py
@@ -49,11 +49,39 @@ train_data, test_data = orders_transformed.randomSplit([0.75, 0.25], seed = 43)
 
 forest = RandomForestRegressor(featuresCol='features', labelCol='quantity_ordered')
 
-forest_model = forest.fit(train_data)
+'''forest_model = forest.fit(train_data)
 
 predictions = forest_model.transform(test_data)
 
 evaluator = RegressionEvaluator(labelCol='quantity_ordered', predictionCol='prediction', metricName='rmse')
 rmse = evaluator.evaluate(predictions)
 
-print(f"RMSE: {rmse}") ##0.411
+print(f"RMSE: {rmse}") ##0.411'''
+
+evaluator = RegressionEvaluator(labelCol='quantity_ordered', predictionCol='prediction', metricName='rmse')
+##Melhorando o modelo
+
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
+
+#Definindo o ParamGrid
+
+grid = ParamGridBuilder().addGrid(forest.numTrees, [100, 200, 300]) \
+                        .addGrid(forest.maxDepth, [5, 10, 15]) \
+                        .build()
+                        
+##Definindo o crossvalidator
+
+crossval = CrossValidator(estimator = forest, estimatorParamMaps=grid, evaluator = evaluator, numFolds = 3)
+
+##Treinamento do modelo
+cv_model = crossval.fit(train_data)
+
+##Melhor modelo
+best_model = cv_model.bestModel
+best_prediction = best_model.transform(test_data)
+
+##Acessando a avaliação do melhor modelo
+rmse = evaluator.evaluate(best_prediction)
+
+print("Melhor modelo:", best_model)
+print("RMSE:", rmse)
